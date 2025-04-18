528027e167b4cd2a98bbd0991aa6388c3354b34b
karthik333711 <139526674+karthik333711@users.noreply.github.com>
2025-04-18 12:16:20 +0530

    Update 1.bat

--- 1.bat
+++ 1.bat
@@ -1,1 +1,23 @@
+@echo off
+:loop
+ping DC01.lab.COM  -n 1 | find "TTL=" > nul
+if errorlevel 1 (
+    echo Cannot Connect Server DCA4.SCASA4.COM, retrying...
+    timeout /t 1 > nul
+    goto loop
+) else (
+    echo  Server Connected successfully!
+    REM Go to A: drive
+    goto A:
+)
+
+:A
+
+
+powershell -Command "Start-Process PowerShell -ArgumentList '-ExecutionPolicy Bypass -NoExit -File ""%~dp01.ps1""' -Verb RunAs"
+
+
+
+timeout /t 1 && exit
+
 
